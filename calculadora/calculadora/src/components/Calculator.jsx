6dde0e337c062b55d887e4070d55a5597ef25050
Paulo Junior <paulojpjunior@gmail.com>
2024-07-11 19:24:50 -0300

    Calculator
    Lógica para o click dos números e limpeza do display.

--- calculadora/calculadora/src/components/Calculator.jsx
+++ calculadora/calculadora/src/components/Calculator.jsx
@@ -3,17 +3,42 @@ import { useState } from "react";
 import "./Calculator.css";
 
 const Calculator = () => {
+  const [currentValue, setCurrentValue] = useState("0");
+  const [pendingOperation, setPendingOperation] = useState(null);
+  const [pendingValue, setPendingValue] = useState(null);
+  const [completeOperation, setCompleteOperation] = useState("");
+
   const keypadNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const operations = ["+", "-", "*", "/"];
 
+  const handleClick = (val) => {
+    setCurrentValue((prevValue) => {
+      if (prevValue === "0") {
+        return val;
+      } else {
+        return prevValue + val;
+      }
+    });
+    setCompleteOperation((prevOperation) => prevOperation + val);
+  };
+
+  const handleClear = () => {
+    setCurrentValue("0");
+    setPendingOperation(null);
+    setPendingValue(null);
+    setCompleteOperation("");
+  };
+
   return (
     <div className="calculator">
-      <div className="complete-operation"></div>
-      <div className="display"></div>
+      <div className="complete-operation">{completeOperation}</div>
+      <div className="display">{currentValue}</div>
       <div className="buttons">
-        <button>AC</button>
+        <button onClick={handleClear}>AC</button>
         {keypadNumbers.map((num) => (
-          <button key={num}>{num}</button>
+          <button key={num} onClick={() => handleClick(num)}>
+            {num}
+          </button>
         ))}
         {operations.map((operation) => (
           <button key={operation}>{operation}</button>
